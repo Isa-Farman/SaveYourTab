218093bdf77a7bfd5c38921e093903e90bf6051b
Isa Farman <isafarman10@gmail.com>
2025-09-17 16:06:14 +0530

    deleted unneeded code

--- index.js
+++ index.js
@@ -9,32 +9,19 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
 
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputbtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deletebtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-
-if(leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
 
 deletebtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-
 })
 
 
 inputbtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
+    console.log(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
 })
 
 function render(leads){
