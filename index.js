3bf3eca38e79c5d841fc330779ebf95b91481590
Isa Farman <isafarman10@gmail.com>
2025-09-17 12:19:00 +0530

    commit initial files

---
+++ index.js
@@ -0,0 +1,52 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputbtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deletebtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabbtn = document.getElementById("tab-btn")
+
+
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+deletebtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+
+})
+
+
+inputbtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+tabbtn.addEventListener("click", function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+         myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads){
+    listItems = ""
+    for( i=0; i < leads.length; i++ ){
+        listItems += `
+            <li>
+            <a  
+                target = '_blank' href='  ${leads[i]} '> ${leads[i]}   
+            </a>
+        </li>
+        `  
+    }
+    ulEl.innerHTML=listItems
+}
+
